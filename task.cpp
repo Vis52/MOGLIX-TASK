7dcbe148443e40cfa89e6c44b0c075474d9b262f
vis52 <vs6773817@gmail.com>
2026-07-22 17:23:49 +0530

    run

--- task.cpp
+++ task.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 #include <string>
-#include <stack>
-#include <algorithm>
 using namespace std;
-
 class Solution {
 public:
+    bool isValid(string s) {
+        int balance = 0;
+        for (char c : s) {
+            if (c == '(') balance++;
+            else balance--;
+              if (balance < 0) return false;
+        }
+        return balance == 0; }
+    
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
+        int n = s.length();
         int maxLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
-                st.push(i);
-            } else {
-                st.pop();
-                if (st.empty()) {
-                    st.push(i);
-                } else {
-                    maxLen = max(maxLen, i - st.top());
+    for (int i = 0; i < n; i++) {
+            for (int j = i + 2; j <= n; j += 2) { 
+                string sub = s.substr(i, j - i);
+                if (isValid(sub)) {
+                    maxLen = max(maxLen, j - i);
                 }
             }
         }
-        
-        return maxLen;
-    }
+        return maxLen;}
 };
 
 int main() {
     Solution sol;
-    cout << sol.longestValidParentheses("(()") << endl;     
+    cout << sol.longestValidParentheses("(()") << endl;      
     cout << sol.longestValidParentheses(")()())") << endl;    
-    cout << sol.longestValidParentheses("())((()))") << endl;          
+    cout << sol.longestValidParentheses("()(()())") << endl;         
     return 0;
-}
+};
